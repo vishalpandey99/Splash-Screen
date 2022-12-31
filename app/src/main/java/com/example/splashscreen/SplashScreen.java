2be1cecb79ff2f1ece3f28c1ee56641fc0d91618
Vishal Pandey <vishalmimit999@gmail.com>
2022-12-31 13:22:23 -0500

    SplashCommit

---
+++ app/src/main/java/com/example/splashscreen/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent iHome = new Intent(SplashScreen.this,MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(iHome);
+                finish();
+            }
+        },4000);
+    }
+}
